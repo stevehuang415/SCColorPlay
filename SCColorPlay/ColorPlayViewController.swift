df3982dfd6370ee7bc06a721fd14855b17cda19d
Steve Huang <shaobaihuang@gmail.com>
2019-12-09 10:15:26 +0800

    code update no auto layout

--- SCColorPlay/ColorPlayViewController.swift
+++ SCColorPlay/ColorPlayViewController.swift
@@ -26,64 +26,18 @@ class ColorPlayViewController: UIViewController {
     var redValue:Float = 255
     var greenValue:Float = 255
     var blueValue:Float = 255
-    var alphaValue:Float = 10
+    var alphaValue:Float = 1
     var randomAlpha:Int = 0
     
     //拉動 RedSlider Change Value 後呼叫 ColorChange()
     @IBAction func RedSliderVC(_ sender: UISlider) {
-        redValue = RedSlider.value
-        ColorChage()
-    }
-    //拉動 GreenSlider Change Value 後呼叫 ColorChange()
-    @IBAction func GreenSlicderVC(_ sender: UISlider) {
-        greenValue = GreenSlider.value
-        ColorChage()
-    }
-    //拉動 BlueSlider Change Value 後呼叫 ColorChange()
-    @IBAction func BlueSliderVC(_ sender: UISlider) {
-        blueValue = BlueSlider.value
-        ColorChage()
-    }
-    //拉動 AlphaSlider Change Value 後呼叫 ColorChange()
-    @IBAction func AlphaSliderVC(_ sender: UISlider) {
-        alphaValue = AlphaSlider.value
-        ColorChage()
-    }
-    //把變更後的Value帶入
-    func ColorChage(){
         
-        //轉換顯示樣式
-        randomAlpha = Int(alphaValue * 10)
-        RedNum.text = "\(Int(redValue))/255"
-        GreenNum.text = "\(Int(greenValue))/255"
-        BlueNum.text = "\(Int(blueValue))/255"
-        AlphaNum.text = "\(Float(randomAlpha)/10)"
+        RedNum.text = "\(Int(RedSlider.value))/255"
+        GreenNum.text = "\(Int(GreenSlider.value))/255"
+        BlueNum.text = "\(Int(BlueSlider.value))/255"
+        AlphaNum.text = "\(String(format: "%.1f" ,AlphaSlider.value))"
         
-        SCImage.backgroundColor = UIColor(displayP3Red: CGFloat(redValue/255), green: CGFloat(greenValue/255), blue: CGFloat(blueValue/255), alpha: CGFloat(alphaValue))
-    }
-    //初始化介面
-    func initColor(){
-        SCImage.backgroundColor = UIColor(displayP3Red:1, green: 1, blue: 1, alpha: 1)
-        
-        RedSlider.tintColor = UIColor(displayP3Red: 255/255, green: 0, blue: 0, alpha: 1)
-        GreenSlider.tintColor = UIColor(displayP3Red: 0, green: 255/255, blue: 0, alpha: 1)
-        BlueSlider.tintColor = UIColor(displayP3Red: 0, green: 0, blue: 255/255, alpha: 1)
-        AlphaSlider.tintColor = UIColor(displayP3Red: 154/255, green: 154/255, blue: 154/255, alpha: 1)
-        
-        RedSlider.value = 255
-        GreenSlider.value = 255
-        BlueSlider.value = 255
-        AlphaSlider.value = 10
-        
-        RedNum.textColor = UIColor(displayP3Red: 255/255, green: 0, blue: 0, alpha: 1)
-        GreenNum.textColor = UIColor(displayP3Red: 0, green: 255/255, blue: 0, alpha: 1)
-        BlueNum.textColor = UIColor(displayP3Red: 0, green: 0, blue: 255/255, alpha: 1)
-        AlphaNum.textColor = UIColor(displayP3Red: 154/255, green: 154/255, blue: 154/255, alpha: 1)
-        
-        RedNum.text = "255/255"
-        GreenNum.text = "255/255"
-        BlueNum.text = "255/255"
-        AlphaNum.text = "1.0"
+        SCImage.backgroundColor = UIColor(displayP3Red: CGFloat(RedSlider.value/255), green: CGFloat(GreenSlider.value/255), blue: CGFloat(BlueSlider.value/255), alpha: CGFloat(AlphaSlider.value))
         
     }
     
@@ -121,7 +75,31 @@ class ColorPlayViewController: UIViewController {
         
     }
     
-    
+    //初始化介面
+    func initColor(){
+        SCImage.backgroundColor = UIColor(displayP3Red:1, green: 1, blue: 1, alpha: 1)
+        
+        RedSlider.tintColor = UIColor(displayP3Red: 255/255, green: 0, blue: 0, alpha: 1)
+        GreenSlider.tintColor = UIColor(displayP3Red: 0, green: 255/255, blue: 0, alpha: 1)
+        BlueSlider.tintColor = UIColor(displayP3Red: 0, green: 0, blue: 255/255, alpha: 1)
+        AlphaSlider.tintColor = UIColor(displayP3Red: 154/255, green: 154/255, blue: 154/255, alpha: 1)
+        
+        RedSlider.value = 255
+        GreenSlider.value = 255
+        BlueSlider.value = 255
+        AlphaSlider.value = 10
+        
+        RedNum.textColor = UIColor(displayP3Red: 255/255, green: 0, blue: 0, alpha: 1)
+        GreenNum.textColor = UIColor(displayP3Red: 0, green: 255/255, blue: 0, alpha: 1)
+        BlueNum.textColor = UIColor(displayP3Red: 0, green: 0, blue: 255/255, alpha: 1)
+        AlphaNum.textColor = UIColor(displayP3Red: 154/255, green: 154/255, blue: 154/255, alpha: 1)
+        
+        RedNum.text = "255/255"
+        GreenNum.text = "255/255"
+        BlueNum.text = "255/255"
+        AlphaNum.text = "1.0"
+        
+    }
     /*
      // MARK: - Navigation
      
